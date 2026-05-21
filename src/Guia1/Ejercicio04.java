bfcd3c332f9c3683ebf7467c3d1d96ebe31f5371
emilio <romeroemilio208@gmail.com>
2026-05-21 00:03:08 -0300

    Ejercicio 4

---
+++ src/Guia1/Ejercicio04.java
@@ -0,0 +1,11 @@
+package Guia1;
+
+// Escribir un metodo llamado mezclarArreglos que toma 2 arreglos de enteros del  mismo tamaño y devuelve
+// un nuevo arreglo del mismo tamaño que el de los de entrada que contiene los elementos de posiciones impares del
+// primer arreglo y los de posiciones pares del  segundo arreglo.
+
+public class Ejercicio04 {
+    public static int[] mezclarArreglos(int[] a, int[] b){
+
+    }
+}
