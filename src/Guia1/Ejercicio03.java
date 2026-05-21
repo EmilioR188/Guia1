9dc86d56faac8fb428355aad03dc941881dd24d4
emilio <romeroemilio208@gmail.com>
2026-05-20 23:09:45 -0300

    Ejercicio 3

--- src/Guia1/Ejercicio03.java
+++ src/Guia1/Ejercicio03.java
@@ -5,5 +5,21 @@ package Guia1;
 // par del arreglo son verdaderos por la cantidad de elementos del arreglo  que en una posición impar son falsos.
 
 public class Ejercicio03 {
-
+    public static int productoBooleanos(boolean[] a){
+        int parPositivo = 0;
+        int imparNegativo = 0;
+        for (int i = 0; i<a.length; ++i){
+            if (esPar(i) && a[i]){
+                parPositivo++;
+            } else {
+                if (!esPar(i) && !a[i]){
+                    imparNegativo++;
+                }
+            }
+        }
+        return parPositivo * imparNegativo;
+    }
+    public static boolean esPar(int indice){
+        return indice % 2 == 0;
+    }
 }
